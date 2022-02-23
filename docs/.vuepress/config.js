ce5fba35b90672894627a774dc81063168d7ccd9
Arjun Chamyal <arjun.chamyal@zeeve.io>
2022-02-23 21:59:50 +0530

    Changes added on Wed Feb 23 21:59:50 IST 2022 by abhi

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: 'Frontier',
+  description: 'Ethereum compatibility layer for Axlib',
+  base: '/frontier/',
+
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  themeConfig: {
+    repo: 'https://github.com/axia-tech/frontier',
+    editLinks: false,
+    docsDir: 'docs',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      { text: 'API reference', link: 'https://axia-tech.github.io/frontier/rustdocs/pallet_evm' }
+    ],
+    sidebar: [
+      'overview',
+      'frame/evm',
+      'frame/ethereum'
+    ]
+  },
+
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
